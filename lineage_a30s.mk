42f05ea5039fac55816aecfd8b9c791066746150
Amy Grace <54113654+00p513-dev@users.noreply.github.com>
2020-10-25 09:59:25 +0000

    Update lineage_a30s.mk

--- lineage_a30s.mk
+++ lineage_a30s.mk
@@ -19,7 +19,7 @@ PRODUCT_RELEASE_NAME := a30s
 $(call inherit-product, build/target/product/embedded.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := a30s
 PRODUCT_NAME := lineage_a30s
